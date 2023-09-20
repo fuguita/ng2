15030447d6d86ecc2a5d1b51701ed3f0b6ea8670
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-09-20 12:23:17 +0000

    genre 途中

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,28 @@
 class Admin::GenresController < ApplicationController
+  
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to request.referer
+  end
+  
+  def new
+    @genge = Genre.new
+  end  
+    
+  def index
+    @genres = Genre.all
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end  
+  
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update
+    redirect_to 
+  end
+  
+  
 end
