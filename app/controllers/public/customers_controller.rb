532e155c477f19f30a05c9fc39ad749b5220e1e4
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-09-23 11:53:19 +0000

    customer edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -6,17 +6,21 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    current_customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def withdarw
+    
   end
 
   private
 
   def customer_params
-    params.require(:customers).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
