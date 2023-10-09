c905feb248b263ae0a7c1e1195746e0d66fa4921
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-10-09 10:22:36 +0000

    shipinng_methods

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,7 +6,27 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
-    @order = Oreder.new(order_params)
+        @order = Oreder.new(order_params)
+      if params[:order][:payment_method] == nill
+         render :new
+         return
+      end
+      if params[:order][:address_number] == "0"
+         @order.customer_address_display = current_customer.customer_address_display
+         @order.name = current_customer.full_name
+         @order.customer_id = current_customer.id
+      elsif params[:order][:address_number] == "1"
+         @shipping = Shipping.find(params:[:order][:shipping_id])
+         @order.shipping_display = @shipping.shipping_display
+      elsif params[:order][:address_number] == "2"
+        if order_params[:postal_code] == "" || order_params[:name] == "" || order_params[:address] == ""
+          render :new
+          return
+        end
+      end  
+       
+      
+    
 
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,13 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
 
   validates :payment_method, presence: true
+  
+  def customer_address_display
+  '〒' + postal_code + ' ' + address
+  end
+  
+  def shipping_display
+    '〒' + "" +  postal_code  + "" + address + "" + name
+  end
 
 end
