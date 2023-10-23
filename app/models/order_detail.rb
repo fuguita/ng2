ef1b6a672293af4df5f63ce2e0e3bac0316b1149
EC2 Default User <126998671+fuguita@users.noreply.github.com>
2023-10-23 12:05:18 +0000

    orderdetail4

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,7 +3,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :food
   belongs_to :order
 
-  enum making_status: { just_ordered: 0, wait_making: 1, making: 2, made_up: 3 }
+  enum making_status: { just_ordered: 0, wait_makings: 1, making: 2, made_up: 3 }
 
 
 end
